92d8d686d7404876a2503502018543f56f67d097
oscarodon <oscar.odon@kavak.com>
2022-02-02 15:24:10 -0300

    Initial Commit

---
+++ CleanUIKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanUIKitApp
+//
+//  Created by Dev on 02/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
